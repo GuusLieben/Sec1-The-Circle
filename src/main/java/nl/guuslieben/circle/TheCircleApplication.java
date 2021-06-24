92ef04bbfaca74558eb648554c24c3f0f6fd971a
Guus Lieben <guus@xendox.com>
2021-06-24 11:29:59 +0200

    Initial product setup

---
+++ src/main/java/nl/guuslieben/circle/TheCircleApplication.java
@@ -0,0 +1,13 @@
+package nl.guuslieben.circle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheCircleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheCircleApplication.class, args);
+    }
+
+}
